05b7316498a3cbec984654f35857a35131996eec
Ritesh Gupta <rg.riteshh@gmail.com>
2015-03-20 13:34:15 +0530

    added basic setup

--- RefreshPaginationManager-Swift/ViewController.swift
+++ RefreshPaginationManager-Swift/ViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+  @IBOutlet weak var tableView: UITableView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,15 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
 
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "cell")
+    cell.textLabel?.text = "cell";
+    return cell
+  }
+  
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 10;
+  }
 
 }
 
