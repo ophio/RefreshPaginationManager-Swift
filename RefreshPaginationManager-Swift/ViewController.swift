2b3310c63d46ecf4cd193178daec3d9a7e58ef80
Ritesh Gupta <rg.riteshh@gmail.com>
2015-03-20 13:24:38 +0530

    Initial Commit

---
+++ RefreshPaginationManager-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RefreshPaginationManager-Swift
+//
+//  Created by rhg-fueled on 20/03/15.
+//  Copyright (c) 2015 Ophio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
